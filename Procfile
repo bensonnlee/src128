1e31498908731480cddf4d203416185bc7689629
koza <kozy.boy13@gmail.com>
2022-04-06 08:18:19 -0700

    changed deployment files to conform to Heroku CI

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --worker-tmp-dir /dev/shm --config gunicorn.conf.py api.app:app
+web: uvicorn api.app:app
--- gunicorn.conf.py
+++
@@ -1,4 +0,0 @@
-bind = "0.0.0.0:8080"
-workers = 2
-# Uvicorn's Gunicorn worker class
-worker_class = "uvicorn.workers.UvicornWorker"
